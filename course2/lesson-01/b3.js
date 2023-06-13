bd764b8452a4ecdae0e0a7956edea9d658bfba12
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-06-13 15:19:33 +0700

    update_b3

---
+++ course2/lesson-01/b3.js
@@ -0,0 +1,23 @@
+function checkCount(arrayA) {
+  const countDict = {};
+  let maxCount = 0;
+  for (i = 0; i < arrayA.length; i++) {
+    if (countDict.hasOwnProperty(arrayA[i])) {
+      countDict[arrayA[i]] += 1;
+    } else {
+      countDict[arrayA[i]] = 1;
+    }
+
+    if (maxCount <= countDict[arrayA[i]]) {
+      maxCount = countDict[arrayA[i]];
+    }
+  }
+
+  const bigValues = Object.keys(countDict).filter(
+    (key) => countDict[key] === maxCount
+  );
+
+  for (i = 0; i < bigValues.length; i++) {
+    console.log(`{value: ${bigValues[i]}, count: ${maxCount}}`);
+  }
+}
