158ddfc968b332b79acb0960ed57b66415c01a0e
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-07-10 18:51:50 +0700

    update

--- course2/final-project/client/src/App.jsx
+++ course2/final-project/client/src/App.jsx
@@ -1,10 +1,16 @@
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className="head-text">Hello!</h1>
-    </div>
+    <main className="app transition-all-ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
