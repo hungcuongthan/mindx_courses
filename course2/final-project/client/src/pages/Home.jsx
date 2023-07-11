8dd1a3a39a2b4902b21c467130d47ecf28780147
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-07-11 18:45:38 +0700

    update

--- course2/final-project/client/src/pages/Home.jsx
+++ course2/final-project/client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { CustomButton } from "../components";
 
 import {
   headContainerAnimation,
@@ -12,7 +13,37 @@ import {
 
 const Home = () => {
   const snap = useSnapshot(state);
-  return <div>Home</div>;
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                Let's <br className="xl:block hidden" /> DO IT!
+              </h1>
+            </motion.div>
+            <motion.div>
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your own shirt!.{" "}
+                <strong>Unleash your imagination</strong> and define your own
+                style.
+              </p>
+
+              <CustomButton />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
