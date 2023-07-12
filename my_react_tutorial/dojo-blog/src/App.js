dffaffc34fb3d539112528f3f0879ce6f4eaf287
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-07-12 18:59:25 +0700

    update

---
+++ my_react_tutorial/dojo-blog/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const title = "Miele";
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
