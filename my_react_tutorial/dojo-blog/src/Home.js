62d200dec073b826ec39465d0720d73a8e655dc1
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-07-14 18:47:22 +0700

    update

--- my_react_tutorial/dojo-blog/src/Home.js
+++ my_react_tutorial/dojo-blog/src/Home.js
@@ -1,29 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((e) => {
-          setError(e.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   const [name, setName] = useState("mario");
 
---
+++ my_react_tutorial/dojo-blog/src/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((e) => {
+          setError(e.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
