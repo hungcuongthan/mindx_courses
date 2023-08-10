aceacf727ce3e593f14ca915ecf92ffb6babcb2a
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2023-08-10 22:35:49 +0700

    update homepage

--- nextjs_tutorial/share_prompts/app/page.jsx
+++ nextjs_tutorial/share_prompts/app/page.jsx
@@ -5,11 +5,13 @@ const Home = () => (
     <h1 className="head_text text-center">
       Discover & Share
       <br className="max-md:hidden" />
-      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      <span className="orange_gradient text-center">
+        Prompts for AI-powered Applications
+      </span>
     </h1>
     <p className="desc text-center">
-      SharePrompt is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      SharePrompt is a platform for modern world to discover, create and share
+      creative prompts
     </p>
 
     <Feed />
